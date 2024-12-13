59d497c6a856340faae24af895c086d43a5a54fc
tobimonges <tobiasmonges@pm.me>
2024-12-13 15:26:29 -0300

    Añadiendo todos los archivos

---
+++ src/main/java/com/postulantes/roshka/RoshkaApplication.java
@@ -0,0 +1,13 @@
+package com.postulantes.roshka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoshkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoshkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postulantes/roshka/RoshkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postulantes.roshka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoshkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
